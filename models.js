7f861943a39db7f8e8248112c49d83c0f1c5039e
Alexandra Havia <sasha.havia@gmail.com>
2018-06-18 19:54:03 -0400

    Finished seed file

--- models.js
+++ models.js
@@ -3,42 +3,37 @@ const db = new Sequelize('postgres://localhost:5432/plantr', {logging: false});
 
 const Gardener = db.define('gardener', {
   name: {
-    type: Sequelize.STRING,
-    allowNull: false
+    type: Sequelize.STRING
   },
   age: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
+    type: Sequelize.INTEGER
   }
 });
 
 const Plot = db.define('plot', {
   size: {
-    type: Sequelize.INTEGER,
-    allowNull: false
+    type: Sequelize.INTEGER
   },
   shaded: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
+    type: Sequelize.BOOLEAN
   }
 });
 
 const Vegetable = db.define('vegetable', {
   name: {
-    type: Sequelize.STRING,
-    allowNull: false
+    type: Sequelize.STRING
   },
   color: {
-    type: Sequelize.STRING,
-    allowNull: false
+    type: Sequelize.STRING
   },
   planted_on: {
-    type: Sequelize.DATE,
-    allowNull: false,
+    type: Sequelize.DATE
   }
 });
 
 Plot.belongsTo(Gardener);
+Gardener.hasOne(Plot);
+
 Vegetable.belongsToMany(Plot, {through: 'vegetable_plot'});
 Plot.belongsToMany(Vegetable, {through: 'vegetable_plot'});
 Gardener.belongsTo(Vegetable, {as: 'favorite_vegetable'});
--- seed.js
+++ seed.js
@@ -3,23 +3,23 @@ const { db, Gardener, Plot, Vegetable } = require('./models');
 db.sync( {force: true} )
   .then( () => {
     return Vegetable.bulkCreate([
-      {name: 'Tomatoe', color: 'red' , planted_on: new Date() },
-      {name: 'Cucumber' , color: 'green' , planted_on: new Date() },
-      {name: 'Potatoe' , color: 'yellow' , planted_on: new Date()}
-    ]);
-  }).then (vegetables => {
-    console.log("Vegies", vegetables);
+      {name: 'Tomatoe', color: 'red', planted_on: new Date() },
+      {name: 'Cucumber' , color: 'green', planted_on: new Date() },
+      {name: 'Potatoe' , color: 'yellow', planted_on: new Date() }
+    ], { returning: true });
+  }).then (createdVegetables => {
+    console.log("Vegies", createdVegetables);
     return Gardener.bulkCreate([
-      {name: 'Tom', age: 35 , favoriteVegetableId: vegetables[0].id },
-      {name: 'Sam' , age: 48 , favoriteVegetableId: vegetables[1].id },
-      {name: 'Michael' , age: 60 , favoriteVegetableId: vegetables[2].id }
-    ]);
-  }).then (gardeners => {
+      {name: 'Tom', age: 35 , favoriteVegetableId: createdVegetables[0].id },
+      {name: 'Sam' , age: 48 , favoriteVegetableId: createdVegetables[1].id },
+      {name: 'Michael' , age: 60 , favoriteVegetableId: createdVegetables[2].id }
+    ], { returning: true });
+  }).then (createdGardeners => {
     return Plot.bulkCreate([
-      {size: 10 , shaded: true , gardenerId: gardeners[0].id },
-      {size: 15 , shaded: false , gardenerId: gardeners[1].id },
-      {size: 35 , shaded: true , gardenerId: gardeners[2].id }
-    ]);
+      {size: 10 , shaded: true , gardenerId: createdGardeners[0].id },
+      {size: 15 , shaded: false , gardenerId: createdGardeners[1].id },
+      {size: 35 , shaded: true , gardenerId: createdGardeners[2].id }
+    ], { returning: true });
   }).catch(err => {
     console.log(err);
   })
